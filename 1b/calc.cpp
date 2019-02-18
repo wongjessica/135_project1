bde6dcdc0840cface6b6bfb8206f3808d6f9a942
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2019-02-18 02:51:44 -0500

    Create calc.cpp

---
+++ 1b/calc.cpp
@@ -0,0 +1,36 @@
+/*
+Author: Jessica Wong
+Course: CSCI-136
+Instructor: Zamansky / Maryash
+Assignment: Project1B
+The program calc.cpp that outputs the solution, a mathematical formula composed of + and -.
+*/
+#include <iostream>
+int main() { //the main function
+
+    char symbol;
+    int total;
+    int number;
+
+    std::cin >> total; //the first value is set as total
+
+    while(std::cin>>symbol>>number) { //while there is an operator + value
+        switch(symbol) { 
+            case '-':
+                total-=number;
+                break;
+            case '+':
+                total+=number;
+                break;
+            default:
+                std::cout<<"Error"<<symbol<<std::endl; //if there is an arbitrary symbol
+                break;
+        }
+    }
+
+    std::cout << total << '\n';
+
+
+
+    return 0; //to assure the code worked correctly
+}
